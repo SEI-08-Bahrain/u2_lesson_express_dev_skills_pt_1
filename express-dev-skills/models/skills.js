5449960b2317c07ee77dbb24f588714780cc5da1
Hamad-Alfandi <Hamad.alfandi7@gmail.com>
2024-01-29 22:42:03 +0300

    complete HW

--- express-dev-skills/models/skills.js
+++ express-dev-skills/models/skills.js
@@ -2,46 +2,17 @@ const skills = [
   {
     id: 1,
     name: "John Developer",
-    skills: {
-      programmingLanguages: ["Python", "JavaScript", "Java"],
-      webDevelopment: {
-        frontend: ["React", "Angular", "Vue"],
-        backend: ["Node.js", "Django", "Spring Boot"],
-      },
-      database: ["MongoDB", "MySQL", "Redis"],
-      cloudServices: ["AWS", "Azure", "Google Cloud"],
-      devOps: ["Docker", "Kubernetes", "Jenkins"],
-      versionControl: ["Git", "GitHub", "Bitbucket"],
-      testing: ["Jest", "Selenium", "JUnit"],
-      mobileDevelopment: ["React Native", "Swift", "Kotlin"],
-      security: ["OAuth", "SSL/TLS", "OWASP Top 10"],
-      agileMethodologies: ["Scrum", "Kanban", "Agile"],
-    },
+    skills: 'React',
   },
   {
     id: 2,
     name: "Jane Coder",
-    skills: {
-      tools: ["VS Code", "IntelliJ IDEA", "Sublime Text"],
-      frameworks: ["Express.js", "Flask", "Spring MVC"],
-      designPatterns: ["MVC", "Singleton", "Observer"],
-      buildTools: ["Webpack", "Gradle", "Maven"],
-      containerization: ["Docker", "Podman"],
-      continuousIntegration: ["Jenkins", "Travis CI", "CircleCI"],
-      monitoring: ["Prometheus", "Grafana", "ELK Stack"],
-      collaboration: ["Slack", "Microsoft Teams", "Jira"],
-    },
+    skills: 'Express.js',
   },
   {
     id: 3,
     name: "Alex Developer",
-    skills: {
-      softSkills: ["Communication", "Problem Solving", "Team Collaboration"],
-      projectManagement: ["Agile", "Scrum", "Kanban"],
-      gitWorkflow: ["Feature Branching", "Pull Requests", "Code Reviews"],
-      documentation: ["API Documentation", "Code Comments", "Readme Files"],
-      learning: ["Self-Taught", "Online Courses", "Workshops"],
-    },
+    skills: 'Python',
   },
 ]
 
